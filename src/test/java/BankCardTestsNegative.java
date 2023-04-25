f3a44382aa14fd220a5200a8aa60d14d030a21b2
Gvynhartt <savshush134@gmail.com>
2023-04-25 05:20:55 +0300

    tests revised according to comments

--- src/test/java/BankCardTestsNegative.java
+++ src/test/java/BankCardTestsNegative.java
@@ -20,13 +20,11 @@ class BankCardTestsNegative {
 
     @BeforeAll
     static void setupAll() {
-        //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver_win32/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setup() {
-        //driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -43,46 +41,75 @@ class BankCardTestsNegative {
     @Test
     void shdTestNameSurnameFieldWithInvalidInput() {
         driver.get("http://localhost:9999/");
-        List<WebElement> inputControls = driver.findElements(By.className("input__control"));
-
-        inputControls.get(0).sendKeys("abobus3450d");
-        inputControls.get(1).sendKeys("+10050013371453");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.xpath("//span[@data-test-id='name']/descendant::input[@name='name']")).sendKeys("abobus3450d");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']/descendant::input[@name='phone']")).sendKeys("+78005553535");
+        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        List<WebElement> inputSubs = driver.findElements(By.className("input__sub"));
-        String actual = inputSubs.get(0).getText();
+        String actual = driver.findElement(By.xpath("//span[contains(@class,'input_invalid')]/descendant::span[@class='input__sub']")).getText();
         assertEquals(expected, actual);
     }
 
     @Test
     void shdTestPhoneNumberFieldWithInvalidInput() {
         driver.get("http://localhost:9999/");
-        List<WebElement> inputControls = driver.findElements(By.className("input__control"));
-
-        inputControls.get(0).sendKeys("Киктор Вислый");
-        inputControls.get(1).sendKeys("+10050013371453");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.xpath("//span[@data-test-id='name']/descendant::input[@name='name']")).sendKeys("Киктор Вислый-Анков");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']/descendant::input[@name='phone']")).sendKeys("+100500");
+        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        List<WebElement> inputSubs = driver.findElements(By.className("input__sub"));
-        String actual = inputSubs.get(1).getText();
+        String actual = driver.findElement(By.xpath("//span[contains(@class,'input_invalid')]/descendant::span[@class='input__sub']")).getText();
         assertEquals(expected, actual);
     }
 
     @Test
     void shdWithTestBothFieldsCorrectButNoCheckbox() {
         driver.get("http://localhost:9999/");
-        List<WebElement> inputControls = driver.findElements(By.className("input__control"));
+        driver.findElement(By.xpath("//span[@data-test-id='name']/descendant::input[@name='name']")).sendKeys("Киктор Вислый-Анков");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']/descendant::input[@name='phone']")).sendKeys("+78005553535");
+        // пропускаем нажатие на метку чекбокса
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
+        // Теперь пропуск галки проверяется, как всё остальное - через X-Path по наличию класса ошибки в разметке. И да, я сравниваю с null (300 IQ).
+        boolean expected = true;
+        boolean actual = false;
+        if ( driver.findElement(By.xpath("//label[contains(@class,'input_invalid')]/descendant::span[@class='checkbox__text']")) != null ) {
+            actual = true;
+        }
+        assertEquals(expected, actual);
+    }
 
-        inputControls.get(0).sendKeys("Киктор Вислый");
-        inputControls.get(1).sendKeys("+78005553535");
-        // нажатие на галку пропускаем, само собой
-        driver.findElement(By.className("button_view_extra")).click();
-        List<WebElement> gdeGalkaTvar = driver.findElements(By.className("input_invalid"));
+    @Test
+    void shdTestFormSubmitWithNameFieldEmpty() {
+        driver.get("http://localhost:9999/");
+        // пропускаем ввод имени
+        driver.findElement(By.xpath("//span[@data-test-id='phone']/descendant::input[@name='phone']")).sendKeys("+78005553535");
+        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.xpath("//span[@data-test-id='name'][contains(@class,'input_invalid')]/descendant::span[@class='input__sub']")).getText();
+        assertEquals(expected, actual);
+    }
 
-        String expected = "rgba(255, 92, 92, 1)";
-        String actual = gdeGalkaTvar.get(0).getCssValue("color");
+    @Test
+    void shdTestFormSubmitWithNumberFieldEmpty() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.xpath("//span[@data-test-id='name']/descendant::input[@name='name']")).sendKeys("Киктор Вислый-Анков");
+        // пропускаем ввод номера
+        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.xpath("//span[@data-test-id='phone'][contains(@class,'input_invalid')]/descendant::span[@class='input__sub']")).getText();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    void shdTestFormSubmitWithZeroInput() {
+        driver.get("http://localhost:9999/");
+        // пропускаем ввод во все поля и нажатие на галку
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.xpath("//span[@data-test-id='name'][contains(@class,'input_invalid')]/descendant::span[@class='input__sub']")).getText();
         assertEquals(expected, actual);
     }
 }
--- src/test/java/BankCardTestsPositive.java
+++ src/test/java/BankCardTestsPositive.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BankCardTestsPositive {
@@ -20,13 +21,11 @@ class BankCardTestsPositive {
 
     @BeforeAll
     static void setupAll() {
-        //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver_win32/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setup() {
-        //driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -41,14 +40,15 @@ class BankCardTestsPositive {
     }
 
     @Test
-    void shdTestShortPathCorrect() {
+    void shdTestShortPathCorrect() throws InterruptedException {
         driver.get("http://localhost:9999/");
-        List<WebElement> inputControls = driver.findElements(By.className("input__control"));
+        driver.findElement(By.xpath("//span[@data-test-id='name']/descendant::input[@name='name']")).sendKeys("Киктор Вислый-Анков");
+        // Добавлена двайная фамилия. Ещё ходят слухи, что поиск по X-Path стильнее, моднее и молодёжнее
+        driver.findElement(By.xpath("//span[@data-test-id='phone']/descendant::input[@name='phone']")).sendKeys("+78005553535");
+        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
 
-        inputControls.get(0).sendKeys("Киктор Вислый");
-        inputControls.get(1).sendKeys("+78005553535");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button_view_extra")).click();
+
+        driver.findElement(By.xpath("//div[@class='form-field form-field_size_m form-field_theme_alfa-on-white']/descendant::button[@role='button']")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         assertEquals(expected, actual);
